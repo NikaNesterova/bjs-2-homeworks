564c632488626ddf2fe9a9a20847697db88fb862
nika_nest <nika_nesterova@bk.ru>
2023-06-20 21:20:31 +0200

    Only the first task is done

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,22 @@
-﻿
+﻿function parseCount(value) {
+    const parsedValue = Number.parseFloat(value);
+    if (isNaN(parsedValue)) {
+      throw new Error("Невалидное значение");
+    }
+    return parsedValue;
+  }
+  
+  function validateCount(value) {
+    try {
+      return parseCount(value);
+    } catch (error) {
+      return error;
+    }
+  }
+
+  console.log(parseCount("123")); // Output: 123
+console.log(parseCount("abc")); // Throws an error: "Невалидное значение"
+
+console.log(validateCount("456")); // Output: 456
+console.log(validateCount("def")); // Output: Error: "Невалидное значение"
+
